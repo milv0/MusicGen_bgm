1555420deb761c51522bba70ecd5d49eded55694
박달루 <127670741+milv0@users.noreply.github.com>
2024-07-25 14:12:29 +0900

    Update musicgen.py

--- musicgen.py
+++ musicgen.py
@@ -1,32 +1,35 @@
-# python.py
+# 필요한 라이브러리 import
 from audiocraft.models import MusicGen
 from audiocraft.models import MultiBandDiffusion
 import math
 import torchaudio
 import torch
-from audiocraft.utils.notebook import display_audio
 from audiocraft.data.audio import audio_write
 import boto3
 
+# MultiBandDiffusion 디코더 사용 여부 설정
 USE_DIFFUSION_DECODER = False
-# Using small model, better results would be obtained with `medium` or `large`.
+
+# MusicGen 모델 초기화 (large 모델 사용)
 model = MusicGen.get_pretrained('facebook/musicgen-large')
 if USE_DIFFUSION_DECODER:
     mbd = MultiBandDiffusion.get_mbd_musicgen()
 
+# 오디오 생성 파라미터 설정
 model.set_generation_params(
-    use_sampling=True,
-    top_k=250,
-    duration=30
+    use_sampling=True,  # 샘플링 기반 생성 방식 사용
+    top_k=250,  # 상위 250개 토큰만 고려
+    duration=30  # 생성할 오디오 길이 30초
 )
 
-# AWS S3 클라이언트 초기화
+# AWS S3 클라이언트 초기화 및 버킷 이름 설정
 s3 = boto3.client('s3')
-bucket_name = 'chatbot-flask'  # 여기에 S3 버킷 이름 입력
+bucket_name = 'chatbot-flask'  # S3 버킷 이름
 
+# 440Hz 비프 소리 생성 함수
 def get_bip_bip(bip_duration=0.125, frequency=440,
                 duration=0.5, sample_rate=32000, device="cuda"):
-    """Generates a series of bip bip at the given frequency."""
+    """440Hz 비프 소리를 생성하는 함수"""
     t = torch.arange(
         int(duration * sample_rate), device="cuda", dtype=torch.float) / sample_rate
     wav = torch.cos(2 * math.pi * 440 * t)[None]
@@ -34,16 +37,35 @@ def get_bip_bip(bip_duration=0.125, frequency=440,
     envelope = (tp >= 0.5).float()
     return wav * envelope
 
-# Here we use a synthetic signal to prompt both the tonality and the BPM
-# of the generated audio.
+'''
+joy 
+hope 
+neutrality
+sadness 
+anger
+anxiety 
+tiredness
+regret
+'''
+
 res = model.generate_continuation(
-    get_bip_bip(0.125).expand(2, -1, -1),
-    32000, ['Jazz jazz and only jazz',
-            'Heartful EDM with beautiful synths and chords'],
+    get_bip_bip(0.125).expand(8, -1, -1),  # prompt의 길이는 3
+    32000, [
+        'Joyful upbeat music with cheerful melodies and harmonies',
+        'Inspirational music with uplifting melodies and motivational lyrics' ,
+        'Calm and serene ambient music for relaxation and meditation' ,
+        'Melancholic and emotional ballads with heartfelt lyrics' ,
+        'Intense and aggressive rock music with heavy riffs and powerful drums' ,
+        'Atmospheric and unsettling ambient music with tense and suspenseful vibes' ,
+        'Soothing and gentle lullabies with calming melodies and soft harmonies' ,
+        'Melancholic and introspective music with somber melodies and reflective lyrics', 
+    
+    ],
     progress=True)
-display_audio(res, 32000)
+
+# 생성된 오디오 파일 저장 및 S3 업로드
 for idx, one_wav in enumerate(res):
-    # Will save under {idx}.wav, with loudness normalization at -14 db LUFS.
+    # 오디오 파일 저장 ({idx}.wav)
     audio_write(f'{idx}', one_wav.cpu(), model.sample_rate, strategy="loudness", loudness_compressor=True)
 
     # S3에 WAV 파일 업로드
